443c5e24398d786a69cacba178f7c199234a38ed
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-03 18:25:58 -0700

    changed

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,6 +1,26 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: log10(n) where n is the number
+# Space complexity: constant - only a certain number of variables are being saved
+
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil
+  return true if number == 0
+  reverse = 0
+  while number > 0
+    temp = number # can check if temp = reverse
+    digit = number % 10
+    reverse = reverse * 10 + digit
+    number = number / 10
+    # number_even = number * 10 + digit
+    p "temp: #{temp}"
+    p "digit: #{digit}"
+    p "reverse: #{reverse}"
+    p "number: #{number}"
+
+    return true if number == reverse || temp == reverse
+    # return true if number == reverse
+  end
+  return false
 end
+
+p is_palindrome(1234321)
